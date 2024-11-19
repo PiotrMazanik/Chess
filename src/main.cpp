7064327f7344e3ce93d1cc0df2b57976dd025e23
PiotrMazanik <2202552@uad.ac.uk>
2024-11-19 21:18:35 +0000

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+﻿#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Log("SDL initialized successfully!");
+    SDL_Quit();
+    return 0;
+}
