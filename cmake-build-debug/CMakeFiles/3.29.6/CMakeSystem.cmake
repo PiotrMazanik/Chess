89dda9d33f17d94fcba108a58fa4bf2a7eb21e3d
PiotrMazanik <2202552@uad.ac.uk>
2024-11-23 16:51:54 +0000

    Made Project Work On any Machine?

--- cmake-build-debug/CMakeFiles/3.29.6/CMakeSystem.cmake
+++ cmake-build-debug/CMakeFiles/3.29.6/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Windows")
 set(CMAKE_HOST_SYSTEM_VERSION "10.0.26100")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
-include("C:/Users/Arch/source/vcpkg-master/scripts/buildsystems/vcpkg.cmake")
+include("C:/Users/Arch/CLionProjects/ChessGame/vcpkg-master/scripts/buildsystems/vcpkg.cmake")
 
 set(CMAKE_SYSTEM "Windows-10.0.26100")
 set(CMAKE_SYSTEM_NAME "Windows")
